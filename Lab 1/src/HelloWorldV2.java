115149886c33d8c6bf489705896ec7d4a72738d2
Kevin Manning <nachyochez@gmail.com>
2016-01-06 12:09:17 -0800

    Added Lab #1

---
+++ Lab 1/src/HelloWorldV2.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Kevin
+ */
+public class HelloWorldV2 {
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hello, World!\n");
+        System.out.println("Are we there yet?");
+    }
+    
+}
---
+++ Lab 1/src/Welcome.java
@@ -0,0 +1,9 @@
+import java.applet.Applet; // import Applet class
+import java.awt.Graphics; // import Graphics class
+public class Welcome extends Applet
+{
+ public void paint( Graphics g )
+ {
+ g.drawString( "Welcome to Java Programming!",25,25);
+ }
+}
