a227af9fb3f855f075487aa45c06d70ce0332e0e
Kevin Manning <nachyochez@gmail.com>
2016-01-06 10:00:50 -0800

    First Homework

---
+++ Hello World/src/hello/world/HelloWorld.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author Kevin
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    /*
+    
+    r1: Using a program is running a set of instructions. Writing a program is creating a set of instructions.
+    r2: The ram can store data as it's being run, the HDD can store data while not in use.
+    r3: Monitor, Printer, and Speakers give output. Keyboard, Mice, Scanners, and drives take input.
+    r4: Smartphones, like android, are fully-powered computers.
+    r5: Able to work in teams, doesn't require to code to processor specific.
+    r6:
+    a) C:\Users\Kevin\Documents\NetBeansProjects\Hello World\src\hello\world
+    b) C:\Users\Kevin\Documents\NetBeansProjects\Hello World\build\classes\hello\world
+    c) 
+    r7: 39 + 3
+        42
+    r8: HelloWorld!
+    
+    */
+    
+////1.1
+//    public static void main(String[] args) {
+//        // TODO code application logic here'
+//
+//        System.out.println("Beinvenidos!");
+//    }
+////1.2
+//    public static void main(String[] args) {
+//        // TODO code application logic here'
+//        int sum = 0;
+//        for(int i = 1; i < 11; i++)
+//        {
+//            sum += i;
+//            System.out.println(sum);
+//        }
+//   }
+////1.3
+//    public static void main(String[] args) {
+//        // TODO code application logic here'
+//
+//        for (int i = 1; i < 10; i++) {
+//            System.out.println(i*(i+1));
+//        }
+//    }
+//1.4
+//    public static void main(String[] args) {
+//        // TODO code application logic here'
+//        float flBalance = 1000f;
+//        System.out.println("Starting Balance: " + flBalance);
+//        
+//        for (int i = 1; i <= 3; i++) {
+//            flBalance *= 1.05;
+//            System.out.println("Current Balance: " + flBalance + " after " + i + " years.");
+//        }
+//    }
+
+}
